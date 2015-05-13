e3162f2c05567ec3f7e478ea67e2632d8387b36c
MachQ <noobny2@gmail.com>
2015-05-13 08:39:10 +0700

    add employee download delay

--- src/com/H2TFC/H2T_DMS_EMPLOYEE/models/Employee.java
+++ src/com/H2TFC/H2T_DMS_EMPLOYEE/models/Employee.java
@@ -105,7 +105,6 @@ public class Employee extends ParseUser {
         put("locked", locked);
     }
 
-    // TODO: 11. Chi tieu
     public double getRevenueGoal() {
         return getDouble("revenue_goal");
     }
